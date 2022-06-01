1229b73e37ae3c75cc7631994769e18c9598e489
GuKong <GuKong@guigu.con>
2022-06-01 14:17:49 +0800

    hot-fix commit

--- src/main/java/com/guhongyu/git/GitTest01.java
+++ src/main/java/com/guhongyu/git/GitTest01.java
@@ -4,5 +4,8 @@ public class GitTest01 {
     public static void main(String[] args) {
         System.out.println("弄好它的奶头");
         System.out.println("大便");
+
+
+        System.out.println("says");
     }
 }
